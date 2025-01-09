ed5969f152e0f235a52561b6a9523abd6bcb16c1
Natasha <natasha.sebastian19@gmail.com>
2025-01-09 22:38:55 +0000

    update

--- app.py
+++ app.py
@@ -6,12 +6,11 @@ app = Flask(__name__)
 
 # Take the input string from user
 # Get the Property values: Polar Surface Area(PSA), Number of Rotations(Nrot), Hydrogen Bond Donor(HBD), Hydrogen Bond Acceptor(HBA) 
-@app.route("/", methods=["GET"], endpoint="get_properties")
+@app.route("/", methods=["GET"])
 def get_properties():
     smiles_query = request.args.get("query", "")
     if smiles_query!="":
         properties = compute_all(smiles_query)
-    
     return render_template(
         "base.html", smiles_query=smiles_query, Nrot_value = properties['nrot'], HBD_value = properties['hbd'], HBA_value = properties['hba']
     )
