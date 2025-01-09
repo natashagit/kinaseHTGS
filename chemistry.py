7bcfa2e11fb31e7addde13f7602b0a425a1bbe54
fatemah-mukadum <fatemahmukadum@nyu.edu>
2025-01-09 22:39:05 +0000

    removed print statements

--- chemistry.py
+++ chemistry.py
@@ -3,6 +3,7 @@ from rdkit.Chem import Lipinski
 from rdkit.Chem import Draw
 from rdkit.DataStructs.cDataStructs import ExplicitBitVect
 from rdkit.Chem import AllChem
+import py3Dmol
 
 from typing import List
 
@@ -16,29 +17,28 @@ EXAMPLE_COMPOUNDS = [
 # input smiles
 def get_rdkit_object(smiles: str):
     mol = Chem.MolFromSmiles(smiles)
-    print(mol)
     return mol
 
 
 # NRot bonds
 def get_rot_bonds(mol):
     nRot = Lipinski.NumRotatableBonds(mol)
-    print(nRot)
     return nRot
 
 
 # HBD
 def get_HBD(mol):
     hbd = Lipinski.NumHDonors(mol)
-    print(hbd)
     return hbd
 
 
 # HBA
 def get_HBA(mol):
     hba = Lipinski.NumHAcceptors(mol)
-    print(hba)
     return hba
+
+
+# Molecular Weight
 
 
 # conformational search
@@ -49,9 +49,35 @@ def conformer_search(mol):
     mol2 = Chem.addHs(mol)
     params = AllChem.ETKDGv3()
     cids = AllChem.EmbedMultipleConfs(mol2, numconfs, params)
-    print(len(cids))
     res = AllChem.MMFFOptimizeMoleculeConfs(mol2)
     return mol2
+
+
+# get the 3D image
+def MolTo3DView(mol, size=(300, 300), style="stick", surface=False, opacity=0.5):
+    """Draw molecule in 3D
+
+    Args:
+    ----
+        mol: rdMol, molecule to show
+        size: tuple(int, int), canvas size
+        style: str, type of drawing molecule
+               style can be 'line', 'stick', 'sphere', 'carton'
+        surface, bool, display SAS
+        opacity, float, opacity of surface, range 0.0-1.0
+    Return:
+    ----
+        viewer: py3Dmol.view, a class for constructing embedded 3Dmol.js views in ipython notebooks.
+    """
+    assert style in ("line", "stick", "sphere", "carton")
+    mblock = Chem.MolToMolBlock(mol)
+    viewer = py3Dmol.view(width=size[0], height=size[1])
+    viewer.addModel(mblock, "mol")
+    viewer.setStyle({style: {}})
+    if surface:
+        viewer.addSurface(py3Dmol.SAS, {"opacity": opacity})
+    viewer.zoomTo()
+    return viewer
 
 
 # wrap everything
@@ -61,7 +87,6 @@ def compute_all(smiles):
     hbd = get_HBD(obj)
     hba = get_HBA(obj)
     props = {"nrot": nrot, "hbd": hbd, "hba": hba}
-    print(props)
     return props
 
 
