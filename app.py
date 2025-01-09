02f0064c57463e5065b18effab981fb4a0eae82a
Natasha <natasha.sebastian19@gmail.com>
2025-01-09 23:09:49 +0000

    update

--- app.py
+++ app.py
@@ -12,14 +12,10 @@ def get_properties():
     # smiles_query = "COC1=CC(C(C2=C3C=NC=C2)=CN(C3=O)C)=CC(OC)=C1CN4CCN(CC4)CCOCCOCCOC5=CC(C6=C(N=CS6)C)=CC=C5CNC([C@@H]7C[C@H](CN7C([C@@H](NC(C8(CC8)F)=O)C(C)(C)C)=O)O)=O"
     if smiles_query != "":
         properties = compute_all(smiles_query)
-
-    return render_template(
-        "base.html",
-        smiles_query=smiles_query,
-        Nrot_value=properties["nrot"],
-        HBD_value=properties["hbd"],
-        HBA_value=properties["hba"],
-    )
+    # Nrot_value=properties["nrot"],
+    # HBD_value=properties["hbd"],
+    # HBA_value=properties["hba"],
+    return render_template("base.html", smiles_query=smiles_query)
 
 
 # Calculate Solubility of molecule in water
