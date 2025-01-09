5d9f508184da449c53db0ca793c9e0f4104d335e
Natasha <natasha.sebastian19@gmail.com>
2025-01-09 22:57:23 +0000

    update

--- app.py
+++ app.py
@@ -5,20 +5,25 @@ app = Flask(__name__)
 
 
 # Take the input string from user
-# Get the Property values: Polar Surface Area(PSA), Number of Rotations(Nrot), Hydrogen Bond Donor(HBD), Hydrogen Bond Acceptor(HBA) 
+# Get the Property values: Polar Surface Area(PSA), Number of Rotations(Nrot), Hydrogen Bond Donor(HBD), Hydrogen Bond Acceptor(HBA)
 @app.route("/", methods=["GET"])
 def get_properties():
     smiles_query = request.args.get("query", "")
-    if smiles_query!="":
+    # smiles_query = "COC1=CC(C(C2=C3C=NC=C2)=CN(C3=O)C)=CC(OC)=C1CN4CCN(CC4)CCOCCOCCOC5=CC(C6=C(N=CS6)C)=CC=C5CNC([C@@H]7C[C@H](CN7C([C@@H](NC(C8(CC8)F)=O)C(C)(C)C)=O)O)=O"
+    if smiles_query != "":
         properties = compute_all(smiles_query)
+
     return render_template(
-        "base.html", smiles_query=smiles_query, Nrot_value = properties['nrot'], HBD_value = properties['hbd'], HBA_value = properties['hba']
+        "display.html",
+        smiles_query=smiles_query,
+        Nrot_value=properties["nrot"],
+        HBD_value=properties["hbd"],
+        HBA_value=properties["hba"],
     )
 
 
 # Calculate Solubility of molecule in water
 # Render the property values, solubility and image for visualization into html
-
 
 
 # @app.route("/", methods=["GET"])
