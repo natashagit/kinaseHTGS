2fdd2c8306ac84b12e254a9cababa98b8f2d0e0f
Natasha <natasha.sebastian19@gmail.com>
2025-01-10 15:57:24 +0000

    updates with initializations for values

--- app.py
+++ app.py
@@ -9,13 +9,23 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def get_properties():
     smiles_query = request.args.get("query", "")
+    Nrot_value = 0
+    HBD_value = 0
+    HBA_value = 0
     # smiles_query = "COC1=CC(C(C2=C3C=NC=C2)=CN(C3=O)C)=CC(OC)=C1CN4CCN(CC4)CCOCCOCCOC5=CC(C6=C(N=CS6)C)=CC=C5CNC([C@@H]7C[C@H](CN7C([C@@H](NC(C8(CC8)F)=O)C(C)(C)C)=O)O)=O"
     if smiles_query != "":
         properties = compute_all(smiles_query)
-    # Nrot_value=properties["nrot"],
-    # HBD_value=properties["hbd"],
-    # HBA_value=properties["hba"],
-    return render_template("base.html", smiles_query=smiles_query)
+        Nrot_value = properties["nrot"]
+        HBD_value = properties["hbd"]
+        HBA_value = properties["hba"]
+
+    return render_template(
+        "compounds.html",
+        smiles_query=smiles_query,
+        Nrot_value=Nrot_value,
+        HBD_value=HBD_value,
+        HBA_value=HBA_value,
+    )
 
 
 # Calculate Solubility of molecule in water
